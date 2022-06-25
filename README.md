981c1ce55c79dea91802cb95c622ba71ea162785
cwd-k2 <kaito@Magnesia.local>
2022-06-26 04:10:01 +0900

    initial commit: template (vue3 + vuetify3 + vite)

---
+++ README.md
@@ -0,0 +1,21 @@
+# README
+
+## Prerequisite
+
+- Wails installation
+
+```
+$ go install github.com/wailsapp/wails/v2/cmd/wails@latest
+```
+
+## Live Development
+
+```
+$ wails dev
+```
+
+## Building
+
+```
+$ wails build
+```
---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import { ConsoleLog } from "../wailsjs/go/main/App.js";
+</script>
+
+<template>
+  <v-app>
+    <v-main>
+      <v-app-bar rounded></v-app-bar>
+      <div class="my-5 text-center">
+        <h1 class="ma-5">Hello World</h1>
+        <div class="ma-5">
+          <v-btn
+            color="primary"
+            depressed
+            outlined
+            @click="() => ConsoleLog('Button Clicked')"
+            >Button</v-btn
+          >
+        </div>
+        <div class="ma-5">
+          <v-btn icon>
+            <v-icon>mdi-pencil</v-icon>
+          </v-btn>
+        </div>
+      </div>
+      <v-bottom-navigation>
+        <v-btn @click="() => ConsoleLog('Heart Button Clicked')">
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+      </v-bottom-navigation>
+    </v-main>
+  </v-app>
+</template>
